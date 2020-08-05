98ee7857a4b6c2d4c9160ad2e3f29b2542c6dcb6
emballesteros <67758898+emballesteros@users.noreply.github.com>
2020-08-04 19:57:11 -0500

    Initial commit

--- resources/views/layouts/app_header_set.blade.php
+++ resources/views/layouts/app_header_set.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width , initial-scale=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <title>Medical System</title>
+    <title>Telemedicina Colombia</title>
     
     <link rel="shortcut icon" href="{{ url('public/favicon.png') }}" type="image/png">
 	<link rel="stylesheet" type="text/css" href="{{ url('public/css/style.css') }}">
